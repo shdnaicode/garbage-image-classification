6e52a1ca6e937431d6aabe70babebb51c4734794
fiddleswe <smartsoodprasert69@gmail.com>
2025-07-18 17:36:25 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A research project for deep learning on image classification model for garbage classification.',
+    author='Shindanai Sudprasert',
+    license='MIT',
+)
